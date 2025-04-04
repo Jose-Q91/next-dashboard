0435309b92627b8f41b09e19afc041c05eec28a0
Jose-Q91 <josecito.qs91@gmail.com>
2025-04-04 17:11:38 -0300

    transmision

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() { //Loading en algo especial de next basado en react
+    //return <div>Loading...</div>;
+    return <DashboardSkeleton></DashboardSkeleton>
+  }
